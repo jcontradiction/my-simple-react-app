41e32c15065ea59763c75ed7a7c6d1bce7de5f8f
dj <djkwak@gmail.com>
2024-07-28 18:23:04 -0500

    add custom hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
-
+import useCount from './useCount';
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const handleIncrease = () => {
-    setCount(count + 1);
-  };
+  // const handleIncrease = () => {
+  //   setCount(count + 1);
+  // };
+
+  // return (
+  //   <div>
+  //     <p>Count: {count}</p>
+  //     <button onClick={handleIncrease}>Increase</button>
+  //   </div>
+  // );
+  const { count, increment, decrement, reset } = useCount(0);
 
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
